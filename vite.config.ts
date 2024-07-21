d12eb6c5c2ca9f1db29afd45d60fec8f96e4f789
rmxway <rmxway@mail.ru>
2024-07-21 13:26:21 +0300

    Added token for public repos

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-import { hostPath } from '@/src/helpers';
+import { hostPath } from './src/helpers';
 
 const env = loadEnv('', process.cwd(), '');
 
@@ -20,8 +20,6 @@ export default defineConfig({
 		emptyOutDir: true,
 	},
 	define: {
-		'process.env.CLIENT_ID': JSON.stringify(env.CLIENT_ID),
-		'process.env.CLIENT_SECRET': JSON.stringify(env.CLIENT_SECRET),
 		'process.env.TOKEN': JSON.stringify(env.TOKEN),
 	},
 	plugins: [react()],
