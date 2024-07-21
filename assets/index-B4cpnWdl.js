ed8a087524b71a5f9d149c5fe4c28f670dfcb249
rmxway <rmxway@mail.ru>
2024-07-21 12:47:25 +0300

    Updates

--- assets/index-B4cpnWdl.js
+++
@@ -1,1 +0,0 @@
-import{j as a,N as t,h as r}from"./index-Bm74Ycqa.js";const n=()=>a.jsxs(t,{children:[a.jsx(r,{to:"/",end:!0,children:"Main"}),a.jsx(r,{to:"/__graphql",target:"_blank",children:"GraphQL"})]});export{n as Navigation,n as default};
