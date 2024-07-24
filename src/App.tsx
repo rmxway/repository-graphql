17f2e77e9be01eeca27fa2f705620d5290c2307b
rmxway <rmxway@mail.ru>
2024-07-24 18:06:45 +0300

    Mini fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { lazy } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { isDev } from './helpers';
 import { Wrapper } from './styled';
-
-const Navigation = lazy(() => import('@/shared/Navigation'));
 
 function App() {
 	return (
 		<Wrapper>
-			<h1>Repository GraphQL</h1>
-			{isDev ? <Navigation /> : null}
+			<h1>Search Repositories via GraphQL</h1>
+			<br />
 			<Outlet />
 		</Wrapper>
 	);
